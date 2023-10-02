ac6b6a424de01705617307d344ed042fb8c6327a
Mateus <mateuslg2000@gmail.com>
2023-10-02 10:22:30 -0300

    Adding a simple cli

--- main.cpp
+++ main.cpp
@@ -16,8 +16,147 @@
 
 int main(int argc, char **argv)
 {
+	int operation = 0;
+	std::cout << "Esfera --> 1\n";
+	std::cout << "Bloco --> 2\n";
+	std::cout << "Volume --> 3\n";
+	std::cout << "Uniao --> 4\n";
+	std::cout << "Escala --> 5\n";
+	std::cin >> operation;
 
-	OctTree *oct = new OctTree(8);
+	if (operation == 1)
+	{
+		// Testing Sphere
+		OctTree *oct = new OctTree(8);
+		Sphere *sp = new Sphere(5.0, Point3(0.0, 0.0, 0.0));
+		oct->setSolid(sp);
+		oct->initOctTree();
+
+		QApplication app(argc, argv);
+		QWidget *w = new QWidget();
+		GLWidget *gl = new GLWidget(w);
+		gl->setOctTree(oct);
+		// testGL *tgl = new testGL(w);
+		w->setFixedSize(640, 480);
+		QHBoxLayout *layout = new QHBoxLayout(w);
+		layout->addWidget(gl);
+		// layout->addWidget(tgl);
+		w->setLayout(layout);
+
+		w->show();
+
+		app.exec();
+
+		return 0;
+	}
+	if (operation == 2)
+	{
+		// Testing Block
+		OctTree *oct = new OctTree(8);
+		Block *block = new Block(Point3(0., 0., 0.), 2., 8., 10.);
+		oct->setSolid(block);
+		oct->initOctTree();
+
+		QApplication app(argc, argv);
+		QWidget *w = new QWidget();
+		GLWidget *gl = new GLWidget(w);
+		gl->setOctTree(oct);
+		// testGL *tgl = new testGL(w);
+		w->setFixedSize(640, 480);
+		QHBoxLayout *layout = new QHBoxLayout(w);
+		layout->addWidget(gl);
+		// layout->addWidget(tgl);
+		w->setLayout(layout);
+
+		w->show();
+
+		app.exec();
+
+		return 0;
+	}
+	if (operation == 3)
+	{
+		// Testing Volume
+		OctTree *oct = new OctTree(8);
+		Sphere *sp = new Sphere(5.0, Point3(0.0, 0.0, 0.0));
+		oct->setSolid(sp);
+		oct->initOctTree();
+		float sphereVolume = 0.0;
+		oct->treeVolume(&sphereVolume, oct->getRoot());
+		std::cout << "Volume :" << sphereVolume << "\n";
+
+		QApplication app(argc, argv);
+		QWidget *w = new QWidget();
+		GLWidget *gl = new GLWidget(w);
+		gl->setOctTree(oct);
+		// testGL *tgl = new testGL(w);
+		w->setFixedSize(640, 480);
+		QHBoxLayout *layout = new QHBoxLayout(w);
+		layout->addWidget(gl);
+		// layout->addWidget(tgl);
+		w->setLayout(layout);
+
+		w->show();
+
+		app.exec();
+
+		return 0;
+	}
+	if (operation == 4)
+	{
+		// Testing Union
+		OctTree *oct = new OctTree(8);
+		Sphere *sp = new Sphere(5.0, Point3(0.0, 0.0, 0.0));
+		oct->setSolid(sp);
+		Block *block = new Block(Point3(0., 0., 0.), 2., 8., 10.);
+		oct->setSolid(block);
+		oct->initOctTree();
+
+		QApplication app(argc, argv);
+		QWidget *w = new QWidget();
+		GLWidget *gl = new GLWidget(w);
+		gl->setOctTree(oct);
+		// testGL *tgl = new testGL(w);
+		w->setFixedSize(640, 480);
+		QHBoxLayout *layout = new QHBoxLayout(w);
+		layout->addWidget(gl);
+		// layout->addWidget(tgl);
+		w->setLayout(layout);
+
+		w->show();
+
+		app.exec();
+
+		return 0;
+	}
+	if (operation == 5)
+	{
+		// Testing Scale
+		OctTree *oct = new OctTree(8);
+		Sphere *sp = new Sphere(5.0, Point3(0.0, 0.0, 0.0));
+		oct->setSolid(sp);
+		oct->initOctTree();
+		oct->treeScale(0.5, oct->getRoot());
+
+		QApplication app(argc, argv);
+		QWidget *w = new QWidget();
+		GLWidget *gl = new GLWidget(w);
+		gl->setOctTree(oct);
+		// testGL *tgl = new testGL(w);
+		w->setFixedSize(640, 480);
+		QHBoxLayout *layout = new QHBoxLayout(w);
+		layout->addWidget(gl);
+		// layout->addWidget(tgl);
+		w->setLayout(layout);
+
+		w->show();
+
+		app.exec();
+
+		return 0;
+	}
+
+	// OctTree *oct = new OctTree(8);
 
 	// Testing Sphere
 	// Sphere *sp = new Sphere(5.0, Point3(0.0, 0.0, 0.0));
@@ -48,12 +187,12 @@ int main(int argc, char **argv)
 	// oct->initOctTree();
 
 	// Testing Volume
-	Sphere *sp = new Sphere(5.0, Point3(0.0, 0.0, 0.0));
-	oct->setSolid(sp);
-	oct->initOctTree();
-	float sphereVolume = 0.0;
-	oct->treeVolume(&sphereVolume, oct->getRoot());
-	std::cout << "Volume :" << sphereVolume << "\n";
+	// Sphere *sp = new Sphere(5.0, Point3(0.0, 0.0, 0.0));
+	// oct->setSolid(sp);
+	// oct->initOctTree();
+	// float sphereVolume = 0.0;
+	// oct->treeVolume(&sphereVolume, oct->getRoot());
+	// std::cout << "Volume :" << sphereVolume << "\n";
 
 	// oct->treeVolume(&sphereVolume, oct->getRoot());
 	// std::cout << "Volume da esfera " << sphereVolume;
@@ -63,20 +202,20 @@ int main(int argc, char **argv)
 	// 	std::cout << oct->getRoot()->getBox()->getPoint(i);
 	// }
 
-	QApplication app(argc, argv);
-	QWidget *w = new QWidget();
-	GLWidget *gl = new GLWidget(w);
-	gl->setOctTree(oct);
-	// testGL *tgl = new testGL(w);
-	w->setFixedSize(640, 480);
-	QHBoxLayout *layout = new QHBoxLayout(w);
-	layout->addWidget(gl);
-	// layout->addWidget(tgl);
-	w->setLayout(layout);
+	// QApplication app(argc, argv);
+	// QWidget *w = new QWidget();
+	// GLWidget *gl = new GLWidget(w);
+	// gl->setOctTree(oct);
+	// // testGL *tgl = new testGL(w);
+	// w->setFixedSize(640, 480);
+	// QHBoxLayout *layout = new QHBoxLayout(w);
+	// layout->addWidget(gl);
+	// // layout->addWidget(tgl);
+	// w->setLayout(layout);
 
-	w->show();
+	// w->show();
 
-	app.exec();
+	// app.exec();
 
-	return 0;
+	// return 0;
 }
